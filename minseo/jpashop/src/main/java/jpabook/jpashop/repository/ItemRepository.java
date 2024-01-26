2b2b0c247e1bc9e3292744cf2f7a7d38b9588e4a
2522001 <minseo770@gmail.com>
2024-01-27 00:37:15 +0900

    [jpa1-5] 상품 리포지토리 개발

---
+++ minseo/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item); // id가 없으면 신규로 본다.
+        } else {
+            em.merge(item); // id가 있으면 이미 디비에 저장된 엔티티를 수정한다고 본다.
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
