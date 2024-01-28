b80d6a4dc3bab9ae423cf666f5bbeff8a6971e96
2522001 <minseo770@gmail.com>
2024-01-29 00:45:36 +0900

    [jpa1-6] 주문 리포지토리 개발

---
+++ minseo/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
