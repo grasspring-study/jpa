e18774ac6105f3a4011c85927f39e391eba55eb2
2522001 <minseo770@gmail.com>
2024-02-23 12:49:58 +0900

    [jpa-3] 영속성 컨텍스트 2

--- minseo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ minseo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,18 @@ public class JpaMain {
 
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 영속
             System.out.println("=== BEFORE ===");
-            em.persist(member) ;
+            em.persist(member);
             System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L); // select 쿼리를 날리지 않음 -> 1차 캐시에 저장되어 있기 때문이다.
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
