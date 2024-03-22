c81fb4e7d815996e8750a057bf7eb127880dbda3
2522001 <minseo770@gmail.com>
2024-03-22 21:00:50 +0900

    [jpa-7] 상속관계 매핑

--- minseo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ minseo/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,30 +16,19 @@ public class JpaMain {
 
         try {
 
-            // 저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.changeTeam(team); // 연관관계의 주인에게 값을 넣어 주어야 한다.
-            em.persist(member);
+            em.persist(movie);
 
-//            em.flush();
-//            em.clear();
-            // 이 부분을 주석처리하면 영속성 컨텍스트에 의해 1차 캐시에서 find가 실행된다.
-            // 이때 team.getMembers().add(member);를 넣지 않으면 team이 Member List를 가지고 있지 않기 때문에 아래 for문에서 아무것도 출력되지 않는다.
-            // 따라서 양쪽에 값을 다 넣어주는 것이 좋다.
+            em.flush();
+            em.clear();
 
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member> members = findTeam.getMembers();
-
-            System.out.println("==============");
-            for (Member m : members) {
-                System.out.println("m = " + m.getUsername());
-            }
-            System.out.println("==============");
+            Movie findMovie = em.find(Movie.class, movie.getId()); // JPA가 inner join 수행
+            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
