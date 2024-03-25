c483a400186d43ff799d7692792b691cec9af4c9
2522001 <minseo770@gmail.com>
2024-03-25 19:49:28 +0900

    [jpa-7] 실전 예제 4 - 상속관계 매핑

--- minseo/jpabasic/src/main/java/jpabook/jpabasic/JpaMain.java
+++ minseo/jpabasic/src/main/java/jpabook/jpabasic/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpabasic.domain.Book;
 import jpabook.jpabasic.domain.Order;
 import jpabook.jpabasic.domain.Member;
 import jpabook.jpabasic.domain.OrderItem;
@@ -19,8 +20,11 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
